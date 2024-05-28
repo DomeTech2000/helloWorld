763421dafeb5af20b15faa5db1a901ca74da62bd
dom <dom@localhost-live.lan>
2024-05-27 17:10:23 -0700

    added SSSD

--- helloWorld.sh
+++ helloWorld.sh
@@ -1,1 +1,11 @@
 echo "Hello,World1"
+
+echo "
+  SSSSS    SSSSS   SSSSS    DDDDD
+ S     S  S     S  S     S  D    D
+ S        S        S        D     D
+  SSSSS    SSSSS    SSSSS   D     D
+       S        S        S  D     D
+ S     S  S     S  S     S  D    D
+  SSSSS    SSSSS    SSSSS   DDDDD
+"
